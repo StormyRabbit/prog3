76d6b4e07bb2af317b5b7e5e69d5130b52fdcb5f
lasse <larseriksjoblom@gmail.com>
2017-12-05 12:48:31 +0100

    Merge branch 'master' of /home/lasse/Projects/C++/prog3 with conflicts.

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -5,9 +5,12 @@
 #include <SDL.h>
 #include <iostream>
 #include <SDL_image.h>
+#include <bits/ios_base.h>
+#include <ios>
 #include "GameEngine.h"
 #include "System.h"
 #include "Player.h"
+#include "HUDSprite.h"
 
 namespace rootengine {
 //Test med sprite EJ final
@@ -29,11 +32,15 @@ namespace rootengine {
 
 
     void GameEngine::run() {
-        std::string IMG_PATH = "assets/sprites/i-know-c.png";
+        std::string IMG_PATH = "assets/sprites/bg_castle.png";
+        
         SDL_Texture *bgImg = IMG_LoadTexture(sys.getRenderer(), IMG_PATH.c_str());
-        SDL_RenderCopy(sys.getRenderer(), bgImg, NULL, NULL);
+        SDL_RenderCopy(sys.getRenderer(), bgImg, nullptr, nullptr);
+        HUDSprite* testSprite = HUDSprite::getInstance(100, 100, 250, 250, "test");
+        testSprite->draw();
         Sprite* player = Player::getInstance(100,100,70,70);
         player->draw();
+        std::cout << IMG_GetError();
         while (running) {
             SDL_RenderPresent(sys.getRenderer());
             SDL_Event event;
